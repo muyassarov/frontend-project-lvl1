57110bc87cddc8f9b01b3aa266bd470a3ba1f7d5
Behruz Muyassarov <muyassarov@gmail.com>
2021-03-06 15:02:37 +0500

    ninth step done

---
+++ src/games/prime.js
@@ -0,0 +1,26 @@
+import runGame from '../index.js';
+
+const NUMBER_OF_ROUNDS = 3;
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < Math.floor(num / 2); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+const runPrimeGame = () => {
+  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const rounds = [];
+  for (let i = 0; i < NUMBER_OF_ROUNDS; i += 1) {
+    const number = Math.floor(Math.random() * 100) + 1;
+    const answer = isPrime(number) ? 'yes' : 'no';
+    rounds.push([number, answer]);
+  }
+  runGame(rounds, gameDescription);
+};
+
+export default runPrimeGame;
